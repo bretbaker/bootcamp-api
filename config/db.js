29d6033bf8eca69cb6f8e965ea20aaa8cbc02b59
Bret Baker <bretbaker808@gmail.com>
2020-02-12 11:57:43 -0800

    revise env vars

--- config/db.js
+++ config/db.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose');
 
 const connectDB = async () => {
-  const c = await mongoose.connect(process.env.MONGO_URI, {
+  const c = await mongoose.connect(process.env.MONGODB_URI, {
     useNewUrlParser: true,
     useCreateIndex: true,
     useFindAndModify: false,
